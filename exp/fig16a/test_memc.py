df461292a9f6e995c26c729588df4662cd614126
btauro <btauro@node-0.btauro-162616.rmt-pg0.utah.cloudlab.us>
2023-09-05 13:33:20 -0600

    update script

--- exp/fig16a/test_memc.py
+++ exp/fig16a/test_memc.py
@@ -9,18 +9,22 @@ import numpy as np
 import sys
 
 
-DATA_SIZE = 100000000
-GET_OPS   = 100000000
+#DATA_SIZE = 100000000
+#GET_OPS   = 100000000
+DATA_SIZE = 1000000
+GET_OPS   = 1000000
 
 key_suffix = ""
 value_suffix = ""
 second=1000000000
 micro_delay=10
 delay=float(micro_delay/second)
+f = open('log', 'w')
 
 def set_key_value(client):
     for i in range(0, DATA_SIZE):
         client.set(str(i)+key_suffix, str(i)+value_suffix)
+        time.sleep(delay)
 
 
 def get_zipf_key_value(client, a):
@@ -36,13 +40,17 @@ def get_zipf_key_value(client, a):
                 print("verification failed expect " + str(hot_keys[i]%DATA_SIZE)+value_suffix + "found" + str(result))
                 exit(0)
     end = timer()
-    print(" a : "+ str(a) + " : " + str(end - start))
+    f.write(" a : "+ str(a) + " : " + str(end - start)+"\n")
+    f.flush()
 
 def zipf_access_pattern():
     zipfr = [ 1.01, 1.02, 1.04, 1.08, 1.1, 1.12, 1.14, 1.18, 1.2, 1.22, 1.24, 1.28, 1.3 ]
     client = Client('127.0.0.1:9999')
+    f.write("client connected\n")
+    f.flush()
     set_key_value(client)
-    print("setup complete")
+    f.write("setup complete\n")
+    f.flush()
     for a in zipfr:
         get_zipf_key_value(client, a)
 
