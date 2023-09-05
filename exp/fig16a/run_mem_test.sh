46896dec66923f267ab7a8d4388803997ca4a8cd
btauro <btauro@node-0.btauro-162616.rmt-pg0.utah.cloudlab.us>
2023-09-05 16:34:02 -0600

    fig16a scripts added

--- exp/fig16a/run_mem_test.sh
+++ exp/fig16a/run_mem_test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 #rm log.*
+figpath="/home/TrackFM/exp/fig16a"
 sudo pkill -9 main
 ./run.sh & 
 sleep 1200
 echo "memclient"
 python3 test_memc.py &>>log
+mv log scripts/figgen/results/fig16a/TrackFM/
+cd ../../plotgen
+python3 scripts/figgen/fig16a.py $figpath fig16a
