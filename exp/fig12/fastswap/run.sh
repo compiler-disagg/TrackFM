5da398ade1be6a82f78be95845b9d32f12227ee2
btauro <btauro@node-0.btauro-163111.rmt-pg0.utah.cloudlab.us>
2023-09-07 17:42:18 -0600

    fix fig 12 script

--- exp/fig12/fastswap/run.sh
+++ exp/fig12/fastswap/run.sh
@@ -5,32 +5,25 @@ source ../../../runtime/AIFM/aifm/shared.sh
 CFM_PATH=/home/cfm/
 TRACKFM_PATH=/home/TrackFM
 
-figpath="/home/TrackFM/exp/fig12/fatswap"
+figpath="/home/TrackFM/exp/fig12/fastswap"
 
 cd $figpath
 
-cache_sizes=( 1 2 3 4 5 6 7 8 9 10 11 12)
 rm log.*
 
 cp main_sum.cpp main.cpp
-for cache_size in "${cache_sizes[@]}"
-do
-    make clean
-    make -j
-    cp main $CFM_PATH/SUM/sum 
-    cd CFM_PATH
-    ./fig12a.sh
-done
-    mv log.* $TRACKFM_PATH/plotgen/scripts/figgen/results/fig11/fastswap/sum/
+make clean
+make -j
+cp main $CFM_PATH"/SUM/sum"
+cd $CFM_PATH
+./fig12a.sh
+mv log.* $TRACKFM_PATH"/plotgen/scripts/figgen/results/fig11/fastswap/sum/"
 cd $figpath
 ##copy
 cp main_copy.cpp main.cpp
-for cache_size in "${cache_sizes[@]}"
-do
-    make clean
-    make -j
-    cp main $CFM_PATH/COPY/copy 
-    cd CFM_PATH
-    ./fig12b.sh
-done
-    mv log.* $TRACKFM_PATH/plotgen/scripts/figgen/results/fig11/fastswap/copy/
+make clean
+make -j
+cp main $CFM_PATH"/COPY/copy" 
+cd $CFM_PATH
+./fig12b.sh
+mv log.* $TRACKFM_PATH"/plotgen/scripts/figgen/results/fig11/fastswap/copy/"
