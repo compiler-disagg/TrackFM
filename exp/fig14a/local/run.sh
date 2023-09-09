47ca43b6f565bf8b3d8bc60ad4a29ebaf49a3f40
btauro <btauro@node-0.btauro-163738.rmt-pg0.utah.cloudlab.us>
2023-09-09 16:14:00 -0600

    fix local script 14a

--- exp/fig14a/local/run.sh
+++ exp/fig14a/local/run.sh
@@ -11,8 +11,7 @@ cd $figpath
 cp /home/TrackFM/apps/C_dataframe/nyc $figpath
 cp /home/TrackFM/apps/C_dataframe/libdataframe.so $figpath
 cp /home/datasets/all.csv .
-./nyc
-mv log.* $TRACKFM_PATH/plotgen/scripts/figgen/results/fig14a/local/
+./nyc &> $TRACKFM_PATH/plotgen/scripts/figgen/results/fig14a/local/log
 cd  $figpath
 cd ../../../plotgen
 python3 scripts/figgen/fig14a.py $figpath fig14a
