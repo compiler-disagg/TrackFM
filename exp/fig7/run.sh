1a250e3370e640d4a57a80209bd751b7877ee9be
btauro <btauro@node-0.btauro-163738.rmt-pg0.utah.cloudlab.us>
2023-09-13 12:35:39 -0600

    bug fix

--- exp/fig7/run.sh
+++ exp/fig7/run.sh
@@ -67,18 +67,18 @@ do
     rerun_mem_server
     mem=$((cache_size*1024*1024*1024))
     sed "s/constexpr uint64_t local_mem_cache_size.*/constexpr uint64_t local_mem_cache_size = $mem;/g" /home/TrackFM/runtime/inc/carm_runtime.hpp -i
+    make -f make_loop_chunk clean 
     cp  ../compile_bitcodes/libc++_$obj_size"_obj_size"/*.o .
     cp /home/TrackFM/symbol_redefine.sh .
-    make -f make_loop_chunk clean 
     make -f make_loop_chunk -j 
     sudo cp libcarmapp.so /usr/local/lib/
     sudo ldconfig
     run_program_noht ./main 1>log.$cache_size 2>&1    
     rerun_local_iokerneld_noht
     rerun_mem_server
+    make -f make_no_chunk clean 
     cp  ../compile_bitcodes/libc++_$obj_size"_obj_size"/*.o .
     cp /home/TrackFM/symbol_redefine.sh .
-    make -f make_no_chunk clean 
     make -f make_no_chunk -j 
     sudo cp libcarmapp.so /usr/local/lib/
     sudo ldconfig
